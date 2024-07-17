215021201b3ff255664b8087cb21ec5a20a0a929
saharit258 <saharitme@gmail.com>
2024-07-18 00:29:53 +0700

    nav and router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.tsx'
+import Home from './page/Home.tsx'
+import Calculate from './page/Calculate.tsx'
+
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/calculate",
+    element: <Calculate />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/page/Calculate.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Nav from '../components/Nav'
+
+function Calculate() {
+  return (
+    <>
+     <Nav/>
+        <div>Calculate</div>
+    </>
+  )
+}
+
+export default Calculate
